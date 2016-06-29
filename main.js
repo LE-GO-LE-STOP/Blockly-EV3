7260822e6d290e3054942f34273a9fca136981c2
Angus Trau <angus.amtra5@gmail.com>
2016-06-29 22:17:01 +1000

    Added program execution

--- main.js
+++ main.js
@@ -2,26 +2,43 @@
 const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
+const child_process = require("child_process");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 let app = express();
 let http = require("http").Server(app);
 let io = require("socket.io")(http);
+
+let spawn;
+function createNewChildProcess(socket) {
+	spawn = child_process.spawn("python", ["python/main.py"]);
+	console.log("Spawned process");
+	socket.emit("exec_log", "Program started");
+
+	spawn.stdout.on("data", function(data) {
+		let str = (new StringDecoder("utf8")).write(data);
+		console.log(str);
+		socket.emit("exec_log", str);
+	});
+
+	spawn.stderr.on("data", function(data) {
+		let str = (new StringDecoder("utf8")).write(data);
+		console.log(str);
+		socket.emit("exec_log", str);
+	});
+
+	spawn.on("close", function(code) {
+		console.log("Spawn done");
+		socket.emit("exec_log", "Program done");
+		socket.emit("exec_done");
+	});
+}
 
 let hasConnection = false; //Only allow 1 user to be connected at a time
 
 app.get("/host.html", function(req, res) {
 	res.set("Blockly-EV3-On-Brick", true); // Tell the client that it is requesting the page from the ev3 brick.
 	res.send("");
-});
-
-app.post("/run", bodyParser.text(), function(req, res) {
-	fs.writeFile("python.main.py", req.body, function(err) {
-		if (err) {
-			res.status(500).end();
-		} else {
-			res.status(200).send("OK");
-		}
-	});
 });
 
 app.use(express.static("http"));
@@ -43,7 +60,23 @@ io.on("connection", function(socket) {
 	socket.on("disconnect", function() {
 		console.log("User disconnected");
 		hasConnection = false;
-	})
+	});
+
+	socket.on("run", function(data) {
+		fs.writeFile("python/main.py", data, function(err) {
+			if (err) {
+				socket.emit("exec_log", "Error: Can not write program to file!");
+				socket.emit("exec_done");
+			} else {
+				createNewChildProcess(socket);
+			}
+		});
+	});
+
+	socket.on("exec_end", function() {
+		spawn.kill();
+		console.log("Killing spawn...")
+	});
 });
 
 http.listen(3000, function() {
